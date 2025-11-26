83955baa3d920548012d39079888093ab618587f
Trần Đình Khánh Đăng <khanhdangtran2004@gmail.com>
2025-11-26 15:46:08 +0700

    Update 02a_run_ddp.sh

--- scripts/02a_run_ddp.sh
+++ scripts/02a_run_ddp.sh
@@ -1,9 +1,9 @@
-mkdir output
-mkdir output/models
+mkdir -p output
+mkdir -p output/models
 
 CUDA_VISIBLE_DEVICES=0,1  python -m torch.distributed.launch \
-    --nproc_per_node=2 --use_env main.py \
-    --img_size 80 \
+    --nproc_per_node=1 --use_env main.py \
+    --img_size 64 \
     --data_path data/imgs/Seen240_S80F50_TRAIN800 \
     --lr 1e-4 \
     --output_k 240 \
