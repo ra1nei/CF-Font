1b86173f6e4c8b33b59dddb13ddc9fd0614f2eb9
Trần Đình Khánh Đăng <khanhdangtran2004@gmail.com>
2025-12-05 22:33:04 +0700

    Update 03c_copy_basis_subset.sh

--- scripts/03c_copy_basis_subset.sh
+++ scripts/03c_copy_basis_subset.sh
@@ -2,7 +2,7 @@ base_n=10
 
 for FLAG in TRAIN800 TEST5646 FS16
 do
-    basis=basis/B0_K240BS32I1000E200_LR1e-4-wdl0.01_20230426-233306_180_basis_240_id_10.txt
+    basis=/kaggle/working/CF-Font/subset.txt
     in_folder=data/imgs/Seen240_S80F50_${FLAG}
     out_folder=data/imgs/BASIS_S80F50_${FLAG}
 
